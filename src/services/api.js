919d7bda2b4c99b6f51099e9416e66af3fa5d3dc
Thairinck da Silva <thairinck.ap@gmail.com>
2025-04-10 11:23:26 -0300

    Aumento do timeout para 30s

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://qntrl-api.vercel.app/api',
-  timeout: 5000,
+  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
   }
